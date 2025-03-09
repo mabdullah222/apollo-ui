537e6666ad1c387361c4df007a12afd2f93eee30
Muhammad Abdullah <ma7800501@gmail.com>
2025-03-09 23:19:39 +0500

    Added paths for profile and settings page

---
+++ app/(home)/settings/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div>
+            <h1>Page</h1>
+            <p>Page content goes here.</p>
+        </div>
+    );
+}
+
+export default Page;
