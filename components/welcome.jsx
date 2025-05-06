16e24076fe6c38cb64b9bb7142f7ae4f8925bad5
Muhammad Abdullah <ma7800501@gmail.com>
2025-05-06 11:32:22 +0500

    Managed the name

--- components/welcome.jsx
+++ components/welcome.jsx
@@ -3,13 +3,15 @@ import React,{useState} from 'react';
 import HelloCard from './hello-card';
 import CourseScroll from './course-scroll';
 import { Button } from './ui/button';
+import { useUser } from '@clerk/nextjs';
 
 const Welcome = () => {
     const [type,setType]=useState('ongoing')
+    const {user}=useUser()
 
     return (
         <div className='flex flex-col basis-1/2 p-10'>
-            <HelloCard></HelloCard>
+            <HelloCard name={user?.firstName || ""}></HelloCard>
             <div className='mt-5 flex flex-col w-full'>
                 <p className='text-2xl text-black font-extrabold'>Lectures</p>
                 <div className='flex flex-row gap-3 justify-start items-center mt-5'>
